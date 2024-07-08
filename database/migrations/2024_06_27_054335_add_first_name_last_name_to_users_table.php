885406007952272aadcaaa3eaae5eb6a1b13cb6a
manishpg83 <manish.bhuvait@gmail.com>
2024-07-08 08:52:53 +0530

    BB mirgarion error

--- database/migrations/2024_06_27_054335_add_first_name_last_name_to_users_table.php
+++ database/migrations/2024_06_27_054335_add_first_name_last_name_to_users_table.php
@@ -14,8 +14,8 @@ class AddFirstNameLastNameToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('first_name')->after('id');
-            $table->string('last_name')->after('first_name');
+            $table->string('first_name')->nullable()->after('id');
+            $table->string('last_name')->nullable()->after('first_name');
         });
     }
 
